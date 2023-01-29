f97d8c6d5eec92833f64dd7a03cd3051852bc868
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-01-29 19:17:12 +0600

    prps and hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const App=()=> {
+  const [counter,setCounter] = useState(0);
 
-function App() {
+  useEffect(()=>{
+    setCounter(counter);
+  },[])
+  
   return (
+
     <div className="App">
-      
+      <button onClick={()=>setCounter((prevCount)=>prevCount-1)} style={{padding:'20px', backgroundColor:"red" ,borderRadius:"5px",fontSize:"20px",color:"white"}}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount)=>prevCount+1)} style={{padding:'20px', backgroundColor:"Blue" ,borderRadius:"5px",fontSize:"20px",color:"white"}}>+</button>
     </div>
   );
 }
